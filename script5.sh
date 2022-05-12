22be808aca74b25ec6c897b29f082e5bd337c9f3
Ricardo Campos <ricard_halo@live.com.mx>
2022-05-12 01:57:17 -0500

    Putting into practice (scripts) learned

---
+++ script5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -d $1 ]; then
+    echo "Error: El directorio no existe"
+    exit 1
+fi
+
+total=0
+
+for f in `ls $1`; do
+    name="$1/$f"
+    if [ -f $name ]; then
+        bytes=`ls -l $name | cut -d " " -f 5`
+        echo "Fichero $name ocupa $bytes" | tr -s /
+        (( total += bytes ))
+    fi
+done
+
+echo -e "\nTotal bytes: $total"
